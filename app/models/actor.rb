00cb66da1f12068003c5cc472422269ae977cd74
felipe <felipebeltran.nyc@gmail.com>
2020-06-12 02:10:53 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    self.characters.map do |character|
+        show_id = character.show_id 
+        show_name = Show.find_by(id: show_id).name
+        "#{character.name} - #{show_name}"
+    end
+      
+  end
   
 end
